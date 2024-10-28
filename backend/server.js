5d7b53f6cd21f29e2c0e644a2d2b15b5078e03e1
Rahul <rahulthakore23@gmail.com>
2024-10-28 21:04:47 +0530

    add tv and movie route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import { ENV_VARS } from "./config/envVars.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
 dotenv.config();
 
 const app = express();
@@ -12,11 +14,12 @@ const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
+
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
     dbConnect();
