48e619387cd22b6ca3c849564741cf86f15dc428
Rahul <rahulthakore23@gmail.com>
2024-10-29 23:15:04 +0530

    add contollers

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import { dbConnect } from "./config/db.js";
 import dotenv from "dotenv";
 import { ENV_VARS } from "./config/envVars.js";
 import cookieParser from "cookie-parser";
+import { protectRoute } from "./middleware/protectRoute.js";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
+import searchRoutes from "./routes/search.route.js";
 import tvRoutes from "./routes/tv.route.js";
 dotenv.config();
 
@@ -20,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 app.listen(PORT, () => {
     dbConnect();
